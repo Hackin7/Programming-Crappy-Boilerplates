b6ae7f992169cb2f805ab455cc1ca9e919e5cb18
name <zunmun@gmail.com>
2021-06-20 17:44:30 +0800

    Cybersec Bufferoverflow Test

--- General Applications/Game Development/PhaserJS/GMTK Game Jam/phaser_setup.js
+++ General Applications/Game Development/PhaserJS/GMTK Game Jam/phaser_setup.js
@@ -1,4 +1,152 @@
-var config = {
+
+var platforms, death, goals;
+var height = 10;
+var width = 13;
+var nodeSize = 60;
+var map = [
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 2, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 4, 0, 3],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+var mapObjects = [];
+var cursors;
+var keyObj;
+class Map extends Phaser.Scene{
+
+    constructor ()
+    {
+        super('Name');
+    }
+
+    preload ()
+    {
+        this.keys = {}
+        //this.load.image('sky', 'assets/sky.png');
+        //this.load.image('ground', 'assets/platform.png');
+        //this.load.image('star', 'assets/star.png');
+        //this.load.image('bomb', 'assets/bomb.png');
+        //this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+
+
+    create ()
+    {
+        this.joiner = new Joiner(this);
+        let player = null;
+        for (let j=0; j<height; j++){
+    		for (let i=0; i<width; i++){
+    			let x = i*nodeSize; let y = j * nodeSize;
+
+    			let node = null;
+    			if (1 <= map[j][i] && map[j][i] <= 4){
+    				node = new JoinableNode(nodeSize, map[j][i], this.joiner);
+    				node.create(this, x, y);
+    				mapObjects.push(node);
+    			}else if (5 <= map[j][i]){//} && map[j][i] <= 4){
+                    player = new Player(nodeSize, 2, this.joiner);
+    				player.create(this, x, y);
+    				mapObjects.push(player);
+                }
+    		}
+
+            //this.drawKeyboard();
+            this.keys.R = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    	}
+
+        cursors = this.input.keyboard.createCursorKeys();
+      let line = this.add.line(100,0,100, 200, 200, 100, 0xffffff);
+      this.physics.add.existing(line);
+      //line1.body.setImmovable(true);
+      line.body.gravity.y=100;
+      line.body.setSize(100, 100, 0, 0);
+      line.body.setCollideWorldBounds(true);
+
+
+        platforms = this.physics.add.group();
+        //platforms.setImmovable(true);
+        //platforms.add(line1);
+        this.physics.collide(platforms);
+        death = this.physics.add.group()
+        goals = this.physics.add.group()
+        let r = this.restart, m = this.moveon;
+        this.physics.collide(platforms, death,
+            (n1, n2) => {r();}, null, this);
+        this.physics.collide(platforms, goals,
+            (n1, n2) => {m();}, null, this);
+
+
+        console.log(this.physics);
+    	for(let i=0; i<mapObjects.length; i++){
+            platforms.add(mapObjects[i].object);
+            switch(mapObjects[i]){
+                case 3:goals.add(mapObjects[i].object);break;
+                case 4:goals.add(mapObjects[i].object);break;
+            }
+
+            //mapObjects[i].object.body.setCollideWorldBounds(true);
+    		for(let j=i+1;j<mapObjects.length;j++){
+                this.physics.collide(mapObjects[i], mapObjects[j],
+                    (n1, n2) => {console.log("1");r();}, null, this);
+                if (mapObjects[i].state == 2 && mapObjects[j].state == 2){
+                    mapObjects[i].join(mapObjects[j]);
+                    mapObjects[j].join(mapObjects[i]);
+                }
+    		}
+    	}
+
+        this.physics.add.collider(platforms);
+
+
+
+    }
+
+    update(){
+    	mapObjects.forEach(object => {
+    		object.update(this, cursors);
+    	});
+
+    	if ( this.keys.R.isDown){
+            console.log("Refsresh");
+            //this.registry.destroy();
+            //this.events.off();
+            this.restart();
+            //this.scene.start('Name');
+    	}
+
+        for(let i=1; i<mapObjects.length; i++){
+    		for(let j=0;j<i;j++){
+                if (mapObjects[i].selected && mapObjects[j].selected){
+
+
+                }
+    		}
+    	}
+    }
+
+    restart(){
+        this.scene.restart();
+        mapObjects = [];
+    }
+
+    moveon(){
+        console.log("Success!");
+        this.scene.start('Name');
+        mapObjects = [];
+    }
+
+}
+
+
+var game = new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
@@ -9,82 +157,5 @@ var config = {
             debug: false
         }
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-var height = 10;
-var width = 10;
-var nodeSize = 60;
-var map = [
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-var mapObjects = [];
-
-function preload ()
-{
-    //this.load.image('sky', 'assets/sky.png');
-    //this.load.image('ground', 'assets/platform.png');
-    //this.load.image('star', 'assets/star.png');
-    //this.load.image('bomb', 'assets/bomb.png');
-    //this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-}
-
-var cursors;
-var keyObj;
-
-function create ()
-{
-	
-    for (let j=0; j<height; j++){
-		for (let i=0; i<width; i++){
-			let x = i*nodeSize; let y = j * nodeSize;
-			
-			let node = null;
-			if (map[j][i] !== 0){
-				node = new JoinableNode(nodeSize, map[j][i]);
-				node.create(this, x, y);
-				mapObjects.push(node);
-			}
-		}
-	}
-	for(let i=1; i<mapObjects.length; i++){
-		for(let j=0;j<i;j++){
-			//console.log(mapObjects[i].object, mapObjects[j].object);
-			this.physics.add.collider(mapObjects[i].object, mapObjects[j].object);
-		}
-	}
-	
-	cursors = this.input.keyboard.createCursorKeys();
-  let line1 = this.add.line(0,0,100, 200, 200, 100, 0xffffff);
-  this.physics.add.existing(line1);
-  line1.body.gravity.y=300;
- 	
-}
-
-function update(){
-	mapObjects.forEach(object => {
-		object.update(this, cursors);
-	});
-	
-	if ( this.input.keyboard.addKey('R').isDown){
-	  //this.restart();
-	}
-}
-
-
-var game = new Phaser.Game(config);
-
+    scene: [Map,]
+});
--- General Applications/Game Development/PhaserJS/GMTK Game Jam/rectangle.js
+++ General Applications/Game Development/PhaserJS/GMTK Game Jam/rectangle.js
@@ -1,4 +1,33 @@
+class Joiner{
+    constructor(context){
+        this.queue = [];
+        this.context = context;
+    }
+    add(node){
+        this.queue.push(node);
+        if (this.queue.length >= 2){
+            let i=0, j=1;
+            this.queue[i].mouseSelect(()=>null);
+            this.queue[j].mouseSelect(()=>null);
+            console.log("connect");
+            this.queue[i].join(this.queue[j]);
+            this.queue[j].join(this.queue[i]);
 
+
+            let line = this.context.add.line(
+                60+30, 0,
+                this.queue[i].getMidX(), this.queue[i].getMidY(),
+                this.queue[j].getMidX(), this.queue[j].getMidY(),
+                0xffffff);
+            //this.context.physics.add.existing(line);
+            //platforms.add(line);
+            //line.body.setImmovable(true);
+            //line.body.setSize(100, 100, 0, 0);
+
+            this.queue = [];
+        }
+    }
+}
 
 
 class GameObject{
@@ -12,13 +41,18 @@ class Rectangle extends GameObject{
     this.width = width;
     this.height = height;
   }
-  
+
   create(context, x, y){
 	this.object = context.add.rectangle(x, y, this.height, this.width, this.colour);
 	this.object.parent = this;
-	this.object.setInteractive(); 
+	this.object.setInteractive();
   }
-  
+
+  update(context, cursors){
+      if (this.object === null){
+          create(context, 0, 0);
+      }
+  }
   outline(width, colour){
 	  this.object.setStrokeStyle(width, colour);
 	  //console.log("Stroke Style");
@@ -26,7 +60,7 @@ class Rectangle extends GameObject{
 }
 
 class JoinableNode extends Rectangle {
-  constructor(size, state) {
+  constructor(size, state, joiner) {
     let colour = 0xffffff;
     switch(state){
 		  case 1:
@@ -43,29 +77,56 @@ class JoinableNode extends Rectangle {
               break;
 		  default:
               break;
-		  
-	  } 
+
+	  }
     super(colour, size, size);
-    this.state = state; //1 for platform, 2 for player, 3 for goal, 4 for death 
+    this.state = state; //1 for platform, 2 for player, 3 for goal, 4 for death
+    this.pastClicked = 0;
+    this.selected = 0;
+    this.joiner = joiner;
   }
-  
+
+  mouseSelect(handler){
+      this.selected = !this.selected;
+      this.outline(this.selected * 4,0xff0000);
+      if (this.selected){
+         handler(this);
+      }else{
+      }
+  }
+
+  getMidX(){
+      let value = this.object.x + this.width / 2;
+      console.log(value);
+      return this.object.x;
+  }
+  getMidY(){
+      return this.object.y;// + this.height / 2;
+  }
+  // Phaser Methods /////////////////////////////////////////////////////////////
   create(context, x, y){
 	  super.create(context, x, y);
-	  //this.underline(4,0xff0000);
+	  //
 	  context.physics.add.existing(this.object);
-	  let obj = this.object;
+	  let obj = this;
 	  this.object.on('pointerdown', function (pointer) {
-	    console.log('pointerdown'); 
-			this.setStrokeStyle(4,0xff0000);
+          if (!this.parent.pastClicked){
+              this.parent.pastClicked = 1;
+              this.parent.mouseSelect((context)=> {context.joiner.add(obj);});
+
+          }
 	  });
 	  this.object.on('pointerup', function (pointer) {
-			this.setStrokeStyle(0 ,0xff0000);
+          if (this.parent.pastClicked){
+              this.parent.pastClicked = 0;
+          }
 
 	  });
-	 
+
   }
-  
+
   update(context, cursors){
+      super.update(context, cursors)
 	 switch(this.state){
 		  case 1:
               //this.object.setPushable(false);
@@ -75,33 +136,60 @@ class JoinableNode extends Rectangle {
               break;
 		  case 2:
 				//this.object.body.static = false;
-			    this.object.body.gravity.y = 400;
-			  
-			    if (cursors.left.isDown){
-					this.object.body.setVelocityX(-160);
-				}else if (cursors.right.isDown){
-					this.object.body.setVelocityX(160);
-				}else{
-					this.object.body.setVelocityX(0);
-				}
-
-				if (cursors.up.isDown && this.object.body.touching.down)
-				{
-					this.object.body.setVelocityY(-330);
-				}
-				
+			    this.object.body.setImmovable(true);
+			    this.object.body.setVelocityX(this.player.object.body.velocity.x);
+                this.object.body.setVelocityY(this.player.object.body.velocity.y);
+                if (this.object.body.touching.down){
+                    this.object.body.setVelocityY(0);
+                }
               break;
 		  case 3:
+                this.object.body.setImmovable(true);
+                 break;
+          case 4:
+              this.object.body.setImmovable(true);
               break;
 		  default:
               break;
-		  
-	  } 
+
+	  }
+  }
+  join(node){
+      this.state = node.state;
+      switch (node.state){
+          case 2:
+            this.player = node;
+            break;
+      }
   }
 }
 
-
-function joinNodes(node1, node2){
-	
+function cc(dict){
+    return dict.up || dict.left || dict.right || dict.down;
 }
+class Player extends JoinableNode{
+    update(context, cursors){
+	    this.object.body.gravity.y = 400;
 
+	    if (cursors.left.isDown){
+			this.object.body.setVelocityX(-160);
+		}else if (cursors.right.isDown){
+			this.object.body.setVelocityX(160);
+		}else{
+			this.object.body.setVelocityX(0);
+		}
+
+        if (this.object.body.touching.down || this.player.object.body.touching.down
+            //|| cc(this.object.body.checkCollision)
+            //|| cc(this.player.object.body.checkCollision)
+        ){
+            //this.object.body.x = this.object.body.x;
+            this.object.body.setVelocityY(0);
+            if (cursors.up.isDown ){
+                this.object.body.setVelocityY(-330);
+            }
+
+        }
+
+    }
+}
