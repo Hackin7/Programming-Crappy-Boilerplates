1b84d1550d653ad664448151f1d6156b34b24455
root <root@DESKTOP-61F9HO2.localdomain>
2021-08-03 13:34:51 +0800

    Android Build Setups

---
+++ General Applications/Mobile/Native Android/simple-build-tools/sample/src/your/pkg/name/MainActivity.java
@@ -0,0 +1,18 @@
+package your.pkg.name;
+
+import android.app.Activity;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.*;
+
+public class MainActivity extends Activity {
+	RelativeLayout layout;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        layout = (RelativeLayout)RelativeLayout.inflate(this, R.layout.activity_main, null);
+        setContentView(layout);
+	}
+}
